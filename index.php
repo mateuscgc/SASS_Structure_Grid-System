9f4ec8a10267dd227ee71dfa21b4e78f10eef865
Copque <victorcopque@infojr.com.br>
2015-02-24 21:20:24 -0300

    Delete index.php

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once('Lib/dcc.inc');
-require_once('Config/dccRouter.php');
-require_once('App/helpers/RoutesHelpers.php');
-
-$app = new \Master\Base();
-$router = new dccRouter();
-$router->route($_SERVER['REQUEST_URI']);
